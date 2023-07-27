2e646d77e0f75d7b21b2b28567c8c4bda3c10081
engfernandes <gabriel.fernandes.06@outlook.com>
2023-07-27 18:22:25 -0300

    :sparkles: feat: add TitleCard component

---
+++ src/components/TitleCard/TitleCard.tsx
@@ -0,0 +1,60 @@
+import { Box, Card, CardActions } from '@mui/material';
+import { Typography, Button, SearchBar } from '@/components';
+import styles from './TitleCard.module.scss';
+
+interface ITitleCard {
+  variant?: 'withAction' | 'withoutAction';
+  title: string;
+  caption: string;
+  actionCaption: string;
+  actionButtonText: string;
+  handleClick: () => void;
+}
+
+function TitleCard({
+  variant = 'withAction',
+  title,
+  caption,
+  actionCaption,
+  actionButtonText,
+  handleClick,
+}: ITitleCard) {
+  return (
+    <>
+      {variant === 'withAction' ? (
+        <Card className={styles.card}>
+          <Box className={styles.content}>
+            <Typography variant='title' text={title} />
+            <Typography variant='caption' text={caption} />
+          </Box>
+          <CardActions className={styles.actions}>
+            <Typography variant='caption' text={actionCaption} />
+            <Button
+              text={actionButtonText}
+              handleClick={handleClick}
+              isAuxButton={true}
+            />
+          </CardActions>
+        </Card>
+      ) : (
+        <Card className={styles.card}>
+          <Box className={styles.content}>
+            <Typography variant='title' text={title} />
+            <Typography variant='caption' text={caption} />
+          </Box>
+          <CardActions className={styles.actionsSimple}>
+            <SearchBar
+              isAuxSearch={true}
+              id='search'
+              label='Pesquisar'
+              value={''}
+              handleChange={() => undefined}
+            />
+          </CardActions>
+        </Card>
+      )}
+    </>
+  );
+}
+
+export default TitleCard;
