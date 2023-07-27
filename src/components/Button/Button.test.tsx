c9ebdfbfa7857c8a288a20cefeffb3f50e3112da
engfernandes <gabriel.fernandes.06@outlook.com>
2023-07-27 12:09:40 -0300

    :boom: fix: fix test name in Button and scss

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -37,7 +37,7 @@ describe('Button', () => {
     expect(wrapper.prop('variant')).toEqual('contained');
   });
 
-  it('Should render with default prop: isAuxButton = false', () => {
+  it('Should call handleClickMock function once', () => {
     const handleClickMock = jest.fn();
 
     const wrapper = shallow(
