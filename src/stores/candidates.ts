1ef5661dbf8c1b6bd64b4f38546477ab4a107bff
engfernandes <gabriel.fernandes.06@outlook.com>
2023-07-29 19:28:45 -0300

    :sparkles: feat: create chosen candidate page

--- src/stores/candidates.ts
+++ src/stores/candidates.ts
@@ -3,10 +3,12 @@ import { ICandidate } from '@/interfaces/ICandidate';
 
 type State = {
   candidates: [ICandidate];
+  selectedCandidate: ICandidate;
 };
 
 type Action = {
   setCandidates: (candidates: State['candidates']) => void;
+  setSelectedCandidate: (selectedCandidate: State['selectedCandidate']) => void;
 };
 
 const useCandidatesStore = create<State & Action>((set) => ({
@@ -21,8 +23,19 @@ const useCandidatesStore = create<State & Action>((set) => ({
       approved: false,
     },
   ],
+  selectedCandidate: {
+    id: 0,
+    name: '',
+    age: 0,
+    picture: '',
+    skills: [''],
+    jobId: 0,
+    approved: false,
+  },
 
   setCandidates: (candidates) => set(() => ({ candidates: candidates })),
+  setSelectedCandidate: (selectedCandidate) =>
+    set(() => ({ selectedCandidate: selectedCandidate })),
 }));
 
 export default useCandidatesStore;
