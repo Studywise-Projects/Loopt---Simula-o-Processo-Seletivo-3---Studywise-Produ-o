191a5b846adf4a79c29e10d94ed2d6389c6d9fe7
engfernandes <gabriel.fernandes.06@outlook.com>
2023-07-29 13:26:32 -0300

    :sparkles: feat: add index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,66 @@
-import Typography from '@/components/Typography/Typography';
+import { Button, Logo, SelectAutoComplete, Typography } from '@/components';
+import { Box, Stack } from '@mui/material';
+import Image from 'next/image';
+import styles from '@/styles/pages/index.module.scss';
+import useJobsStore from '@/stores/jobs';
+import useGetJobs from '@/services/api/useGetJobs';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Select from '@/components/Select/Select';
 
 export default function Home() {
+  const [jobs, selectedJob, setSelectedJob] = useJobsStore((state) => [
+    state.jobs,
+    state.selectedJob,
+    state.setSelectedJob,
+  ]);
+  const handleChangeSelectedJob = (event: any) =>
+    setSelectedJob(event.target.value);
+
+  const { refetch } = useGetJobs();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    refetch();
+  }, []);
+
   return (
-    <>
-      <Typography variant='header' text='Header' />
-      <Typography variant='title' text='Title' />
-      <Typography variant='subtitle' text='Subtitle' />
-      <Typography variant='subtitle2' text='Subtitle2' />
-      <Typography variant='body' text='Body' />
-      <Typography variant='caption' text='Caption' />
-    </>
+    <Stack className={styles.main}>
+      <Stack className={styles.leftContainer}>
+        <Image
+          src='/assets/img/loopt-background.svg'
+          alt='loopt background'
+          width={512}
+          height={800}
+          className={styles.image}
+        />
+      </Stack>
+      <Stack className={styles.rightContainer}>
+        <Box className={styles.rightContent}>
+          <Logo variant='vertical' />
+          <Typography variant='titleBlue' text='Seja bem-vindo!' />
+          <Typography
+            variant='body'
+            text='Esta é a plataforma oficial da Loopt para gestão de processos seletivos em andamento. Abaixo você pode selecionar o processo desejado e avaliar as candidaturas.'
+          />
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: '32px' }}>
+            <Select
+              id='jobs-select'
+              label='Vaga'
+              options={jobs}
+              value={selectedJob}
+              handleChange={handleChangeSelectedJob}
+            />
+            <Button
+              text='Entrar'
+              handleClick={() => {
+                router.push('login');
+              }}
+            />
+          </Box>
+        </Box>
+      </Stack>
+    </Stack>
   );
 }
