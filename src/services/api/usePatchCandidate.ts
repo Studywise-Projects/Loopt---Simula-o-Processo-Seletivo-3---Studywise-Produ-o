191f1b854648b88fcbf9a1308d4358fbe9a94719
engfernandes <gabriel.fernandes.06@outlook.com>
2023-07-29 23:44:58 -0300

    :boom: fix: fix pages bugs and logics

--- src/services/api/usePatchCandidate.ts
+++ src/services/api/usePatchCandidate.ts
@@ -5,18 +5,22 @@ import useGetCandidates from './useGetCandidates';
 function usePatchCandidate(id: number) {
   const { refetch } = useGetCandidates();
 
-  return useQuery('patchCandidate', () => {
-    axios
-      .patch(`http://localhost:5000/candidates/${id}`, {
-        approved: true,
-      })
-      .then(() => {
-        refetch();
-      })
-      .catch((error) => {
-        return new Error('Erro ao alterar o candidato', error);
-      });
-  });
+  return useQuery(
+    'patchCandidate',
+    () => {
+      axios
+        .patch(`http://localhost:5000/candidates/${id}`, {
+          approved: true,
+        })
+        .then(() => {
+          refetch();
+        })
+        .catch((error) => {
+          return new Error('Erro ao alterar o candidato', error);
+        });
+    },
+    { enabled: false },
+  );
 }
 
 export default usePatchCandidate;
