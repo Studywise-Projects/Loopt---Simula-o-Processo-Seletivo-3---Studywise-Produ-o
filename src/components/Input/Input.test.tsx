1791d2eb07b62f5e329da1007254061c7069226f
engfernandes <gabriel.fernandes.06@outlook.com>
2023-07-31 10:08:59 -0300

    :test_tube: refactoring the Input component tests

--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -1,37 +1,91 @@
-import '@testing-library/jest-dom';
-import { shallow } from 'enzyme';
+import { fireEvent, render } from '@testing-library/react';
 import Input from './Input';
 
 describe('Input', () => {
   it('Should match the snapshot', () => {
-    const wrapper = shallow(
+    const { getByLabelText } = render(
       <Input
-        id='1'
-        label='Input'
+        id='text-input'
+        label='text-label'
         type='text'
         value=''
         handleChange={() => undefined}
+        handleKeyPress={undefined}
       />,
     );
 
-    expect(wrapper).toMatchSnapshot();
+    const element = getByLabelText('text-label');
+    expect(element).toMatchSnapshot();
+  });
+
+  it('Should render correctly', () => {
+    const { getByLabelText } = render(
+      <Input
+        id='text-input'
+        label='text-label'
+        type='text'
+        value=''
+        handleChange={() => undefined}
+        handleKeyPress={undefined}
+      />,
+    );
+
+    const element = getByLabelText('text-label');
+    expect(element).toBeInTheDocument();
+  });
+
+  it('Should render correctly with type = password', () => {
+    const { getByLabelText } = render(
+      <Input
+        id='password-input'
+        label='password-label'
+        type='password'
+        value=''
+        handleChange={() => undefined}
+        handleKeyPress={undefined}
+      />,
+    );
+
+    const element = getByLabelText('password-label');
+    expect(element).toHaveAttribute('type', 'password');
   });
 
   it('Should call handleChangeMock function once', () => {
     const handleChangeMock = jest.fn();
 
-    const wrapper = shallow(
+    const { getByLabelText } = render(
       <Input
-        id='2'
-        label='Input'
+        id='text-input'
+        label='text-label'
         type='text'
         value=''
         handleChange={handleChangeMock}
+        handleKeyPress={undefined}
       />,
     );
 
-    wrapper.find('[data-test-id="input"]').simulate('change', 'Test');
+    const element = getByLabelText('text-label');
+    fireEvent.change(element, { target: { value: 'input changed' } });
+    expect(handleChangeMock).toHaveBeenCalledTimes(1);
+  });
 
-    expect(handleChangeMock).toBeCalledWith('Test');
+  it('Should call handlePressKeyMock function once', () => {
+    const handlePressKeyMock = jest.fn();
+
+    const { getByLabelText } = render(
+      <Input
+        id='text-input'
+        label='text-label'
+        type='text'
+        value=''
+        handleChange={() => undefined}
+        handleKeyPress={handlePressKeyMock}
+      />,
+    );
+
+    const element = getByLabelText('text-label');
+    fireEvent.keyDown(element, { key: 'Enter', code: 'Enter' });
+
+    expect(handlePressKeyMock).toHaveBeenCalledTimes(1);
   });
 });
