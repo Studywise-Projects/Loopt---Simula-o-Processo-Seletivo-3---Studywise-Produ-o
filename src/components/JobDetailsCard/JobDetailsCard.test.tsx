a3f904837a024f54ee101ee5d83f1e8e21a354ed
engfernandes <gabriel.fernandes.06@outlook.com>
2023-07-31 10:19:48 -0300

    :test_tube: refactoring the JobDetailsCard component tests

---
+++ src/components/JobDetailsCard/JobDetailsCard.test.tsx
@@ -0,0 +1,62 @@
+import { fireEvent, render } from '@testing-library/react';
+import JobDetailsCard from './JobDetailsCard';
+import { ChangeEvent } from 'react';
+
+describe('JobDetailsCard', () => {
+  it('Should match the snapshot', () => {
+    const { getByText } = render(
+      <JobDetailsCard
+        job={{
+          id: 1,
+          label: 'Desenvolvedor Front-End',
+          requirements: ['ReactJs', 'NextJs', 'React Native'],
+          differentials: ['Python', 'Figma'],
+        }}
+        value={''}
+        handleChange={() => undefined}
+      />,
+    );
+
+    const element = getByText('ReactJs');
+    expect(element).toMatchSnapshot();
+  });
+
+  it('Should render correctly', () => {
+    const { getByText } = render(
+      <JobDetailsCard
+        job={{
+          id: 1,
+          label: 'Desenvolvedor Front-End',
+          requirements: ['ReactJs', 'NextJs', 'React Native'],
+          differentials: ['Python', 'Figma'],
+        }}
+        value={''}
+        handleChange={() => undefined}
+      />,
+    );
+
+    const element = getByText('Figma');
+    expect(element).toBeInTheDocument();
+  });
+
+  it('Should call handleChangeMock function once', () => {
+    const handleChangeMock = jest.fn();
+
+    const { getByLabelText } = render(
+      <JobDetailsCard
+        job={{
+          id: 1,
+          label: 'Desenvolvedor Front-End',
+          requirements: ['ReactJs', 'NextJs', 'React Native'],
+          differentials: ['Python', 'Figma'],
+        }}
+        value={''}
+        handleChange={handleChangeMock}
+      />,
+    );
+
+    const element = getByLabelText('Buscar candidato pelo nome');
+    fireEvent.change(element, { target: { value: 'value changed' } });
+    expect(handleChangeMock).toHaveBeenCalledTimes(1);
+  });
+});
