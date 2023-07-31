5155ab3c95d994219804642a3f04406c3bd1fcc5
engfernandes <gabriel.fernandes.06@outlook.com>
2023-07-31 10:41:32 -0300

    :test_tube: refactoring the SearchBar component tests

--- src/components/SearchBar/SearchBar.test.tsx
+++ src/components/SearchBar/SearchBar.test.tsx
@@ -1,36 +1,49 @@
-import '@testing-library/jest-dom';
-import { shallow } from 'enzyme';
+import { fireEvent, render } from '@testing-library/react';
 import SearchBar from './SearchBar';
-import { ChangeEvent } from 'react';
 
 describe('SearchBar', () => {
   it('Should match the snapshot', () => {
-    const wrapper = shallow(
+    const { getByLabelText } = render(
       <SearchBar
-        id='1'
-        label='Search'
+        id='search-bar-test'
+        label='test'
         value=''
         handleChange={() => undefined}
       />,
     );
 
-    expect(wrapper).toMatchSnapshot();
+    const element = getByLabelText('test');
+    expect(element).toMatchSnapshot();
   });
 
-  it('Should call handleChangeMock function once', () => {
+  it('Should render correctly', () => {
+    const { getByLabelText } = render(
+      <SearchBar
+        id='search-bar-test'
+        label='test'
+        value=''
+        handleChange={() => undefined}
+      />,
+    );
+
+    const element = getByLabelText('test');
+    expect(element).toBeInTheDocument();
+  });
+
+  it('Should call handleChangeMock function call', () => {
     const handleChangeMock = jest.fn();
 
-    const wrapper = shallow(
+    const { getByLabelText } = render(
       <SearchBar
-        id='1'
-        label='Search'
+        id='search-bar-test'
+        label='test'
         value=''
         handleChange={handleChangeMock}
       />,
     );
 
-    wrapper.find('[data-test-id="searchbar"]').simulate('change', 'Test');
-
-    expect(handleChangeMock).toBeCalledWith('Test');
+    const element = getByLabelText('test');
+    fireEvent.change(element, { target: { value: 'search bar changed' } });
+    expect(handleChangeMock).toHaveBeenCalledTimes(1);
   });
 });
